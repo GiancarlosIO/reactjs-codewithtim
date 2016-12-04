a1a75d3c099b96edb64bae4e59cb0c6a62037173
Giancarlos <giancarlos.isasi@gmail.com>
2016-12-04 01:16:06 -0500

    Add main component

--- src/app/index.js
+++ src/app/index.js
@@ -1,13 +1,50 @@
-var React  = require('react');
+var React = require('react');
 var ReactDOM = require('react-dom');
 
-// Create TodoComponent component
-var TodoComponent = React.createClass({
+var CounterComponent = require('./counterComponent');
+
+var AppComponent = React.createClass({
+  propTypes: {
+    name: React.PropTypes.string.isRequired,
+    location: React.PropTypes.string.isRequired
+  },
+  /*getDefaultProps: function(){
+    return {
+      name: 'Carlos'
+    }
+  },*/
+  getInitialState: function(){
+    return {
+      name: this.props.name,
+      location: this.props.location
+    }
+  },
   render: function(){
     return(
-      <li>{this.props.hello}</li>
-    )
-  }
-})
+      <div>
+        <input type="text" value={this.state.name} onChange={this.nameChange}/>
+        <h1>I'm {this.state.name}</h1>
+        <p>I live in {this.state.location}</p>
+        <button onClick={this.resetName} >Reset name</button>
+        <h1>CounterComponent - counter challenge</h1>
+        <CounterComponent />
+      </div>
+    );
+  },
 
-ReactDOM.render(<TodoComponent hello='Hello' />, document.getElementById('todo-wrapper'))
+  // custom functions
+  nameChange: function(event){
+    this.setState(
+      {
+        name: event.target.value
+      }
+    )
+  },
+  resetName: function () {
+    this.setState({
+      name: 'Giancarlos'
+    })
+  }
+});
+
+ReactDOM.render(<AppComponent name="Giancarlos" location="Ica" />, document.getElementById('todo-wrapper'));
