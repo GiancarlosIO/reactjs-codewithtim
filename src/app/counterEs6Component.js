ea2aaa5188fb79f6179ef46147b5db1704c7183c
Giancarlos <giancarlos.isasi@gmail.com>
2016-12-04 19:46:12 -0500

    Add counter-es6 component

---
+++ src/app/counterEs6Component.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class CounterEs6Component extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <span>im the counteres6 component</span>
+    )
+  }
+}
+
+export default CounterEs6Component;
