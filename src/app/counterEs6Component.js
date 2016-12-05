398431fab5b304739c5d643fd211fd2540461853
Giancarlos <giancarlos.isasi@gmail.com>
2016-12-04 19:57:39 -0500

    Import and call the es6-button component in counter-es6 component

--- src/app/counterEs6Component.js
+++ src/app/counterEs6Component.js
@@ -1,18 +1,31 @@
 import React from 'react';
 
+import CounterEs6ButtonComponent from './counterEs6ButtonComponent';
+
 class CounterEs6Component extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       counter: 0
     }
+    this.increment = this.increment.bind(this);
+  }
+
+  increment(value) {
+    let updateCounter = this.state.counter + value;
+    this.setState({
+      counter: updateCounter
+    })
   }
 
   render() {
     return (
       <div>
         <span>{this.state.counter}</span>
-        
+        <br/>
+        <CounterEs6ButtonComponent number={1} text="Add" handlerClick={()=>this.increment(1)}/>
+        <CounterEs6ButtonComponent number={5} text="Add" handlerClick={()=>this.increment(5)}/>
+        <CounterEs6ButtonComponent number={10} text="Add" handlerClick={()=>this.increment(10)}/>
       </div>
     )
   }
